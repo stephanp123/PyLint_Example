abc3d6f75f56b0378e8db36dcdedff587923b767
stephanp <stephanpopescu123@gmail.com>
2025-09-29 10:26:10 -0400

    Add Factorial.py

---
+++ lint.py
@@ -0,0 +1,20 @@
+#lint.py 
+
+import sys 
+
+from pylint import lint  
+
+THRESHOLD = 9  
+
+run = lint.Run(["Factorial.py"]) 
+
+score = run.linter.stats["global_note"]  
+
+if score < THRESHOLD: 
+
+    print("Linter failed: Score < threshold value") 
+
+    sys.exit(1) 
+
+
+sys.exit(0) 
